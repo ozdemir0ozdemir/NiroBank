617b7ddf6a1ca3d55ac0a7aa9ae740279f69ee9f
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-12-06 14:09:25 +0300

    TokenEntity has turned to jpa entity

--- token-service/src/main/java/ozdemir0ozdemir/nirobank/tokenservice/domain/TokenEntity.java
+++ token-service/src/main/java/ozdemir0ozdemir/nirobank/tokenservice/domain/TokenEntity.java
@@ -1,28 +1,37 @@
 package ozdemir0ozdemir.nirobank.tokenservice.domain;
 
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.Accessors;
 
 import java.time.Instant;
 import java.util.UUID;
 
-@Data
-@AllArgsConstructor
+@Entity
+@Table(name = "tokens")
+@Getter
+@Setter
 @Accessors(chain = true)
+@NoArgsConstructor
 class TokenEntity {
+
+    @Id
+    @SequenceGenerator(name = "tokens_id_gen", sequenceName = "tokens_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "tokens_id_gen", strategy = GenerationType.SEQUENCE)
+    private long id;
     private String tokenId;
     private String username;
     private String token;
     private Instant expiresAt;
     private TokenStatus tokenStatus;
 
-    static TokenEntity of(String username, String token, Instant expiredAt) {
-        return new TokenEntity(
-                username + ":" + UUID.randomUUID().toString(),
-                username,
-                token,
-                expiredAt,
-                TokenStatus.ACCEPTABLE);
+    static TokenEntity of(String username, String token, Instant expiresAt) {
+        return new TokenEntity()
+                .setTokenId(username + ":" + UUID.randomUUID())
+                .setUsername(username)
+                .setToken(token)
+                .setExpiresAt(expiresAt)
+                .setTokenStatus(TokenStatus.ACCEPTABLE);
     }
 
 }
