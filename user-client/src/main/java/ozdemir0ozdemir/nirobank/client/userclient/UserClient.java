b56650ebe9fd46a3daa0cf57d0b25c704fa9e0bb
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-12-07 20:01:32 +0300

    user-client imports optimized

--- user-client/src/main/java/ozdemir0ozdemir/nirobank/client/userclient/UserClient.java
+++ user-client/src/main/java/ozdemir0ozdemir/nirobank/client/userclient/UserClient.java
@@ -1,7 +1,6 @@
 package ozdemir0ozdemir.nirobank.client.userclient;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ozdemir0ozdemir.common.response.PagedResponse;
 import ozdemir0ozdemir.common.response.Response;
