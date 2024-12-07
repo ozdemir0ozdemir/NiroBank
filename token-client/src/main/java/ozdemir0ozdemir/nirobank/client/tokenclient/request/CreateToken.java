d8aac10a192b486265645813915a5fbe89c35889
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-12-07 20:48:40 +0300

    role enum added token-client

--- token-client/src/main/java/ozdemir0ozdemir/nirobank/client/tokenclient/request/CreateToken.java
+++ token-client/src/main/java/ozdemir0ozdemir/nirobank/client/tokenclient/request/CreateToken.java
@@ -1,6 +1,4 @@
 package ozdemir0ozdemir.nirobank.client.tokenclient.request;
 
-import ozdemir0ozdemir.nirobank.client.userclient.Role;
-
 public record CreateToken(String username, Role role) {
 }
---
+++ token-client/src/main/java/ozdemir0ozdemir/nirobank/client/tokenclient/request/Role.java
@@ -0,0 +1,19 @@
+package ozdemir0ozdemir.nirobank.client.tokenclient.request;
+
+import java.util.List;
+
+public enum Role {
+    USER("USER:read"),
+    MANAGER("USER:read", "USER:write", "ADMIN:read"),
+    ADMIN("USER:read", "USER:write", "ADMIN:read", "ADMIN:write");
+
+    private final List<String> permissions;
+
+    Role(String... permissions) {
+        this.permissions = List.of(permissions);
+    }
+
+    public List<String> getPermissions() {
+        return permissions;
+    }
+}
