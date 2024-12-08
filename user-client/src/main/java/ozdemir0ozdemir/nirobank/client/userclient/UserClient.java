2f20fbaf48eb2bf9adc4780ab22262f0ffa3faf3
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-12-08 22:12:59 +0300

    login function added to the user-client

--- user-client/src/main/java/ozdemir0ozdemir/nirobank/client/userclient/UserClient.java
+++ user-client/src/main/java/ozdemir0ozdemir/nirobank/client/userclient/UserClient.java
@@ -1,11 +1,13 @@
 package ozdemir0ozdemir.nirobank.client.userclient;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ozdemir0ozdemir.common.response.PagedResponse;
 import ozdemir0ozdemir.common.response.Response;
 import ozdemir0ozdemir.nirobank.client.userclient.request.ChangeUserPassword;
 import ozdemir0ozdemir.nirobank.client.userclient.request.ChangeUserRole;
+import ozdemir0ozdemir.nirobank.client.userclient.request.Login;
 import ozdemir0ozdemir.nirobank.client.userclient.request.RegisterUser;
 
 @FeignClient(url = "http://"+"${user-service:localhost:8080}"+"/api/v1/users", name = "Users")
@@ -22,6 +24,9 @@ public interface UserClient {
 
     @GetMapping("/{userId}")
     Response<User> getUserByUserId(@PathVariable Long userId);
+
+    @GetMapping("/login")
+    Response<User> login(@RequestBody Login request);
 
     @PatchMapping
     void changeUserRoleByUserId(@RequestBody ChangeUserRole request);
---
+++ user-client/src/main/java/ozdemir0ozdemir/nirobank/client/userclient/request/Login.java
@@ -0,0 +1,4 @@
+package ozdemir0ozdemir.nirobank.client.userclient.request;
+
+public record Login(String username, String password) {
+}
