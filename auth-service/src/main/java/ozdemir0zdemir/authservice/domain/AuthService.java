3c0f3b6aeb7fbdd40d2e869a0ab57f5ca53a938b
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-12-08 22:08:12 +0300

    authservice class implemented

--- auth-service/src/main/java/ozdemir0zdemir/authservice/domain/AuthService.java
+++ auth-service/src/main/java/ozdemir0zdemir/authservice/domain/AuthService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ozdemir0ozdemir.nirobank.client.tokenclient.TokenClient;
 import ozdemir0ozdemir.nirobank.client.userclient.UserClient;
+import ozdemir0ozdemir.nirobank.client.userclient.request.RegisterUser;
 
 @Service
 @RequiredArgsConstructor
@@ -11,4 +12,8 @@ public class AuthService {
 
     private final UserClient userClient;
     private final TokenClient tokenClient;
+
+    public void register(String username, String password) {
+        userClient.registerUser(new RegisterUser(username, password));
+    }
 }
