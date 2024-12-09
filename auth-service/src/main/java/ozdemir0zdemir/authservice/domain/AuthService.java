e65362e8612271dc54e0c829b44fb348fac9fa1f
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-12-09 21:08:56 +0300

    todos added for authservice.java

--- auth-service/src/main/java/ozdemir0zdemir/authservice/domain/AuthService.java
+++ auth-service/src/main/java/ozdemir0zdemir/authservice/domain/AuthService.java
@@ -7,6 +7,7 @@ import ozdemir0ozdemir.common.response.ResponseStatus;
 import ozdemir0ozdemir.nirobank.client.tokenclient.AccessToken;
 import ozdemir0ozdemir.nirobank.client.tokenclient.TokenClient;
 import ozdemir0ozdemir.nirobank.client.tokenclient.request.CreateToken;
+import ozdemir0ozdemir.nirobank.client.tokenclient.request.RefreshToken;
 import ozdemir0ozdemir.nirobank.client.userclient.User;
 import ozdemir0ozdemir.nirobank.client.userclient.UserClient;
 import ozdemir0ozdemir.nirobank.client.userclient.request.Login;
@@ -42,5 +43,20 @@ public class AuthService {
         // TODO: Send request to the message broker instead of sending directly
         // TODO: Send activation code to users email (User model has no email field yet)
         this.userClient.registerUser(new RegisterUser(username, password));
+    }
+
+    public AccessToken refresh(String refreshTokenId) {
+
+        // TODO: parse token, get username and roles to validate user
+        // TODO: send validation request via user-client
+
+        Response<AccessToken> accessTokenResponse =
+                this.tokenClient.refreshAccessToken(new RefreshToken(refreshTokenId));
+
+        if(accessTokenResponse.getStatus().equals(ResponseStatus.FAILED)){
+            throw new RuntimeException(accessTokenResponse.getMessage()); // FIXME: TokenException
+        }
+
+        return accessTokenResponse.getPayload();
     }
 }
