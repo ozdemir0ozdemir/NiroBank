5e41da141ffd062eb205a1ee3cd65376c1834f0e
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-12-14 15:56:35 +0300

    global exception handler extended

--- user-service/src/main/java/ozdemir0ozdemir/userservice/api/GlobalExceptionHandler.java
+++ user-service/src/main/java/ozdemir0ozdemir/userservice/api/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ozdemir0ozdemir.common.response.Response;
+import ozdemir0ozdemir.userservice.exception.UserNotFoundException;
 import ozdemir0ozdemir.userservice.exception.UsernameAlreadyExistsException;
 
 @ControllerAdvice
@@ -15,6 +16,13 @@ class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     ResponseEntity<Response<Void>> handleUsernameAlreadyExistsException(Exception ex) {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
+                .body(Response.failed(ex.getMessage()));
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    ResponseEntity<Response<Void>> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
                 .body(Response.failed(ex.getMessage()));
     }
 }
