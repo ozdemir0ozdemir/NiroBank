aadddb5d602acb67c9f51158ebfa9e2a090d47ea
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-12-15 01:12:17 +0300

    tokenservice bug fixed

--- token-service/src/main/java/ozdemir0ozdemir/nirobank/tokenservice/TokenServiceApplication.java
+++ token-service/src/main/java/ozdemir0ozdemir/nirobank/tokenservice/TokenServiceApplication.java
@@ -2,7 +2,6 @@ package ozdemir0ozdemir.nirobank.tokenservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * In this application, the term 'Token' refers to the 'Refresh Token'.
@@ -10,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  *
  * */
 @SpringBootApplication
-@EnableFeignClients(basePackages = "ozdemir0ozdemir.nirobank.client")
 public class TokenServiceApplication {
 
     public static void main(String[] args) {
