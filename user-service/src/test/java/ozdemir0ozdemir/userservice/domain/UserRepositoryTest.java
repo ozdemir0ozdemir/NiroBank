6f2f87db48847c92dacd7b2b9e7c96c0cb5420e8
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-12-14 15:12:32 +0300

    user-service test PostgresContainer extract to an interface

--- user-service/src/test/java/ozdemir0ozdemir/userservice/domain/UserRepositoryTest.java
+++ user-service/src/test/java/ozdemir0ozdemir/userservice/domain/UserRepositoryTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.jdbc.Sql;
