8e030fd45491b9eebc78f831915cec2b267c051c
ozdemir0ozdemir <ozdemirozdemir@hotmail.com.tr>
2024-12-17 19:08:09 +0300

    token-service test

---
+++ token-service/src/test/java/ozdemir0ozdemir/nirobank/tokenservice/domain/RefreshTokenServiceUnitTest.java
@@ -0,0 +1,9 @@
+package ozdemir0ozdemir.nirobank.tokenservice.domain;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class RefreshTokenService_UnitTest {
+
+}
